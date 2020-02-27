997e9c1bf9806c35d64f73a20166da5e6960c202
opeeny <godfreyopeeny@gmail.com>
2020-02-27 10:28:27 +0000

    Refactor code

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,11 +1,22 @@
-import React, {createContext, useReducer} from 'react';
+import React, {createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 //book context to be consumed
 export const BookContext = createContext();
  
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer, []);
+    const getLocalStorage = () => {
+        const browserData = localStorage.getItem('books');
+        console.log(browserData);
+        return browserData ? JSON.parse(browserData) : [];//if no data, else return empty array
+    };
+    const [books, dispatch] = useReducer(bookReducer, [], getLocalStorage);
+    //use Effect() runs has  a call-back function that runs every-time the data changes
+    useEffect(() => {
+
+        localStorage.setItem('books', JSON.stringify(books));//turning it into json
+
+    }, [books])
     return (
         <BookContext.Provider value={{books, dispatch}}>
             {props.children}
